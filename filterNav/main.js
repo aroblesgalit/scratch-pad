574cb59e92d96e9e4adefae4b2bcbf6591f5c0a5
Alvin Galit <aroblesgalit@gmail.com>
2022-05-14 00:27:10 -0600

    Added comments

--- filterNav/main.js
+++ filterNav/main.js
@@ -1,9 +1,26 @@
 $(document).ready(function () {
+  // Grab all filter items
   const filters = $('.thc-home-filter > li')
+  // For each filter item
   filters.each(function () {
+    // listen to a click event
     $(this).click(function () {
+      // then remove the class 'active' from all of them
       filters.removeClass('active')
+      // and add it to the one that was clicked
       $(this).addClass('active')
+      // Grab all the products
+      const products = $('.thc-home-products > li')
+      // Get the text of the filter that was clicked
+      const filter = $(this).text()
+      // If 'all' was clicked, then show all products
+      if (filter === 'all') {
+        products.show()
+        // else, hide all products, then show all with the class name that matches the filter text
+      } else {
+        products.hide()
+        $(`.${filter}`).show()
+      }
     })
   })
 })
