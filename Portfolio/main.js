10f5b9335d9eae8ac553fadea2c7c3535eb41690
Alvin Galit <aroblesgalit@gmail.com>
2022-05-23 23:17:26 -0600

    Used jquery to render home content

--- Portfolio/main.js
+++ Portfolio/main.js
@@ -1,1 +1,9 @@
-$(document).ready(function () {})
+$(document).ready(function () {
+  const mainDiv = $('main')
+
+  function renderHome () {
+    const html = `<img src="https://via.placeholder.com/575x700" alt="Alvin Galit" />`
+    $('main').append(html)
+  }
+  renderHome()
+})
