68178f0b41566e2844adf06d04c1415f0519e118
Alvin Galit <aroblesgalit@gmail.com>
2022-05-25 23:23:06 -0600

    Created event listeners for touch events

--- Touch_Events/main.js
+++ Touch_Events/main.js
@@ -1,1 +1,15 @@
-$(document).ready(function () {})
+$(document).ready(function () {
+  const mainDiv = $('main')
+
+  mainDiv.on('touchstart', function () {
+    console.log('start')
+  })
+
+  mainDiv.on('touchmove', function () {
+    console.log('move')
+  })
+
+  mainDiv.on('touchend', function () {
+    console.log('end')
+  })
+})
