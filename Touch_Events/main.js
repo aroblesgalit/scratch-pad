14b9f1c7a87b6e212130f374f3443e72ee35aa70
Alvin Galit <aroblesgalit@gmail.com>
2022-05-26 23:11:55 -0600

    Added conditional to only run code insde mousemove and touchmove while value isDown is true

--- Touch_Events/main.js
+++ Touch_Events/main.js
@@ -1,18 +1,23 @@
 $(document).ready(function () {
   const mainDiv = $('main')
+  let isDown = false
 
   mainDiv.on('mousedown touchstart', function (e) {
     e.preventDefault()
+    isDown = true
     console.log('start')
   })
 
   mainDiv.on('mousemove touchmove', function (e) {
     e.preventDefault()
-    console.log('move')
+    if (isDown) {
+      console.log('move')
+    }
   })
 
   mainDiv.on('mouseup touchend', function (e) {
     e.preventDefault()
+    isDown = false
     console.log('end')
   })
 })
