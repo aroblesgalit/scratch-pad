74a1b57bcc4c4e87d263561239d50ce4b523fa2f
Alvin Galit <aroblesgalit@gmail.com>
2022-09-26 20:19:55 -0600

    Added functionality

--- Collapsible/main.js
+++ Collapsible/main.js
@@ -1,2 +1,8 @@
 const collapsibles = document.querySelectorAll(".collapsible");
 console.log(collapsibles);
+
+for (let i = 0; i < collapsibles.length; i++) {
+  collapsibles[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+  });
+}
